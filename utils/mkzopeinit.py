26409efd8b4c69f47a9db32ef0d58915d1bbc912
izak <izak@80044c55-6f29-0410-b7d5-9c2a68987f97>
2008-02-08 14:08:11 +0000

    * python program to create init scripts based on our generic zope.init script
    git-svn-id: https://svn.upfronthosting.co.za/svn/upfront/MonitorTools/trunk@69 80044c55-6f29-0410-b7d5-9c2a68987f97

---
+++ utils/mkzopeinit.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#
+# $Id$
+#
+
+""" Add an init script for a zope cluster
+"""
+
+import sys
+import getopt
+import commands
+
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-d", "--base", help="Base directory where zope cluster lives")
+parser.add_option("-D", "--description", help="Description of the instance")
+parser.add_option("-t", "--timeout", help="How long to wait for a client to restart", default=60)
+parser.add_option("-z", "--zeo", help="Path of zeo relative to base directory", default="zeo")
+parser.add_option("-c", "--clients", help="Path of zeo relative to base directory", default="client1 client2")
+parser.add_option("-u", "--user", help="What user to run zope as", default="zope")
+
+(options, args) = parser.parse_args()
+
+def check_required(attr):
+    if getattr(options, attr, None) is None:
+        parser.error("%s is required" % attr)
+        sys.exit(1)
+
+check_required('base')
+check_required('description')
+
+#fp = open("/etc/init.d/zope_%s"%(options.description), "w")
+fp = open("zope_%s"%(options.description), "w")
+fp.write("ZOPE=%s\n"%(options.base))
+fp.write("DESC=%s\n"%(options.description))
+fp.write("ZOPE_RESTART_TIMEOUT=%s\n"%(options.timeout))
+fp.write("ZEO=%s\n"%(options.zeo))
+fp.write("CLIENTS=%s\n"%(options.clients))
+fp.write("SRVUSER=%s\n\n"%(options.user))
+fp.write("source usr/share/upfrontmonitortools/zope/zope.init")
+fp.close()
